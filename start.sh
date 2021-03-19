956c74a0af419649010cb9088b145b75f2e024ad
Brian Jemilo II <jemiloii@gmail.com>
2021-03-19 15:25:41 -0500

    Update start script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-killall -9 node >> /dev/null;
-cd /root/photostudio/;
+(killall -9 node >> /dev/null &);
+cd /root/photostudio/ || return;
 (node redirect.js >> /dev/null &);
 (npm run dev >> /dev/null &);
 echo "Server Started!";
